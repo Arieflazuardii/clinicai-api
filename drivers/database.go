0782ef2d9f7bd1f332e783f8ea6e53326750fab9
Arieflazuardii <arief.gox@gmail.com>
2023-10-23 16:07:47 +0700

    feat(drivers): add config database

---
+++ drivers/database.go
@@ -0,0 +1,45 @@
+package drivers
+
+import (
+	"clinicai-api/models/schema"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() *gorm.DB {
+	err := godotenv.Load(filepath.Join(".", ".env"))
+	if err != nil {
+		fmt.Println("error loading .env file: ")
+		os.Exit(1)
+	}
+
+	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v?charset=utf8mb4&parseTime=True&loc=Local",
+	os.Getenv("DB_USER"),
+	os.Getenv("DB_PASSWORD"),
+	os.Getenv("DB_HOST"),
+	os.Getenv("DB_PORT"),
+	os.Getenv("DB_NAME"))
+
+	var errDB error
+	DB, errDB = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if errDB != nil {
+		panic("failed connect to database")
+	}
+
+	Migrate()
+
+	fmt.Println("Connected to database")
+
+	return DB
+}
+
+func Migrate() {
+	DB.AutoMigrate(schema.Patient{})
+}
