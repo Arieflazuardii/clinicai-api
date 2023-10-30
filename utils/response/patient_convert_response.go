0f76f128207291fcad9a1118368b9e16f3ea3ee4
Arieflazuardii <arief.gox@gmail.com>
2023-10-31 01:55:13 +0700

    fix(patient): update convert response

--- utils/response/patient_convert_response.go
+++ utils/response/patient_convert_response.go
@@ -15,7 +15,6 @@ func PatientDomainToPatientLoginResponse(patient *domain.Patient) web.PatientLog
 
 func PatientSchemaToPatientDomain(patient *schema.Patient) *domain.Patient {
 	return &domain.Patient{
-		ID:           patient.ID,
 		Name: 	      patient.Name,
 		Email:        patient.Email,
 		Password: 	  patient.Password,
@@ -30,10 +29,8 @@ func PatientSchemaToPatientDomain(patient *schema.Patient) *domain.Patient {
 
 func PatientDomainToPatientResponse(patient *domain.Patient) web.PatientResponse {
 	return web.PatientResponse{
-		ID: 		  patient.ID,
 		Name:         patient.Name,
 		Email:        patient.Email,
-		Password:     patient.Password,
 		Nik:          patient.Nik,
 		Birthday:     patient.Birthday,
 		Age:          patient.Age,
@@ -47,10 +44,8 @@ func ConvertPatientResponse(patients []domain.Patient) []web.PatientResponse {
 	var results []web.PatientResponse
 	for _, patient := range patients {
 		patientResponse := web.PatientResponse{
-			ID:           patient.ID,
 			Name:         patient.Name,
 			Email:        patient.Email,
-			Password:     patient.Password,
 			Nik:          patient.Nik,
 			Birthday:     patient.Birthday,
 			Age:          patient.Age,
