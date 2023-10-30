7249c54d0bc40ea88f505144d222ca040f37a946
Arieflazuardii <arief.gox@gmail.com>
2023-10-31 02:00:56 +0700

    fix(schedule):  update dto response

--- models/web/schedule_response.go
+++ models/web/schedule_response.go
@@ -3,15 +3,23 @@ package web
 import "time"
 
 type ScheduleResponse struct {
-	ID       uint    	`json:"id"`
-	DoctorID uint    	`json:"DoctorID"`
-	Date     time.Time `json:"date" `
-	Quota    int       `json:"quota" `
+	DoctorID uint    	`json:"doctorID"`
+	Date     time.Time `json:"date"`
+	Quota    int       `json:"quota"`
+	DoctorName string `json:"doctor_name"`
+}
+
+type ConvertScheduleResponse struct {
+	DoctorID uint    	`json:"doctorID"`
+	Date     time.Time `json:"date"`
+	Quota    int       `json:"quota"`
+	DoctorName string `json:"doctor_name"`
 }
 
 type ScheduleUpdateResponse struct {
-	ID       uint    	`json:"id"`
 	DoctorID uint   	`json:"DoctorID"`
 	Date     time.Time  `json:"date" `
 	Quota    int        `json:"quota"`
 }
+
+
