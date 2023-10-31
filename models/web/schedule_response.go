a3c0466f30750a24777d4cee927afbaf49d0c4e0
Arieflazuardii <arief.gox@gmail.com>
2023-11-01 01:59:07 +0700

    feat(schedule): update response dto

--- models/web/schedule_response.go
+++ models/web/schedule_response.go
@@ -3,6 +3,7 @@ package web
 import "time"
 
 type ScheduleResponse struct {
+<<<<<<< Updated upstream
 	DoctorID uint    	`json:"doctorID"`
 	Date     time.Time `json:"date"`
 	Quota    int       `json:"quota"`
@@ -17,6 +18,10 @@ type ConvertScheduleResponse struct {
 }
 
 type ScheduleUpdateResponse struct {
+=======
+<<<<<<< Updated upstream
+	ID       uint    	`json:"id"`
+>>>>>>> Stashed changes
 	DoctorID uint    	`json:"DoctorID"`
 	Date     time.Time `json:"date" `
 	Quota    int       `json:"quota" `
@@ -27,3 +32,27 @@ type ScheduleUpdateResponse struct {
 	Date     time.Time  `json:"date" `
 	Quota    int        `json:"quota"`
 }
+=======
+	ID uint `json:"id"`
+	DoctorID uint    	`json:"doctorID"`
+	Date     time.Time `json:"date"`
+	Quota    int       `json:"quota"`
+	DoctorName string `json:"doctor_name"`
+}
+
+type ConvertScheduleResponse struct {
+	ID uint `json:"id"`
+	DoctorID uint    	`json:"doctorID"`
+	Date     time.Time `json:"date"`
+	Quota    int       `json:"quota"`
+	DoctorName string `json:"doctor_name"`
+}
+
+type ScheduleUpdateResponse struct {
+	ID uint `json:"id"`
+	DoctorID uint    	`json:"DoctorID"`
+	Date     time.Time `json:"date"`
+	Quota    int       `json:"quota"`
+	DoctorName string `json:"doctor_name"`
+}
+>>>>>>> Stashed changes
