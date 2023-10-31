6717b6eac7ff69d9175808f6d3fdd6c0b3885b74
Arieflazuardii <arief.gox@gmail.com>
2023-11-01 01:41:27 +0700

    feat(diagnosis): fix response dto

--- models/web/diagnosis_response.go
+++ models/web/diagnosis_response.go
@@ -3,17 +3,20 @@ package web
 import "clinicai-api/models/domain"
 
 type DiagnosisResponse struct {
+	ID uint `json:"id"`
 	RegistrationID uint                `json:"registrationID"`
 	Registration   *domain.Registration `json:"registration"`
 	OpenAIResult   string              `json:"openAIResult"`
 }
 
 type DiagnosisCreateResponse struct {
+	ID uint `json:"id"`
 	RegistrationID uint   `json:"registrationID"`
 	OpenAIResult   string `json:"openAIResult"`
 }
 
 type DiagnosisUpdateResponse struct {
+	ID uint `json:"id"`
 	RegistrationID uint   `json:"registrationID"`
 	OpenAIResult   string `json:"openAIResult"`
 }
